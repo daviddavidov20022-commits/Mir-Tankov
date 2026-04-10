1bff3fc0c1b63d0b0c7acfdfd071f0d552f6e2d3
daviddavidov20022-commits <daviddavidov20022@gmail.com>
2026-04-10 22:03:39 +0300

    fix: build script py2/3 compat, mod v1.1.0 compiled

--- mod_src/build_bounty_mtmod.py
+++ mod_src/build_bounty_mtmod.py
@@ -5,13 +5,13 @@ import os
 src = r"d:\mir-tankov-bot\mod_src\mod_bounty_hunter.py"
 pyc = r"d:\mir-tankov-bot\mod_src\mod_bounty_hunter.pyc"
 py_compile.compile(src, pyc, doraise=True)
-print "Compiled OK"
+print("Compiled OK")
 
 mtmod = r"d:\mir-tankov-bot\mod_src\bounty_hunter_1.0.0.mtmod"
 
 meta_xml = """<root>
     <id>mir.tankov.bounty.hunter</id>
-    <version>1.0.0</version>
+    <version>1.1.0</version>
     <name>BountyHunter</name>
     <description>Tracks damage dealt to streamer for bounty system</description>
 </root>"""
@@ -25,4 +25,4 @@ with zipfile.ZipFile(mtmod, 'w', zipfile.ZIP_STORED) as z:
     z.writestr("res/scripts/client/gui/mods/", "")
     z.write(pyc, "res/scripts/client/gui/mods/mod_bounty_hunter.pyc")
 
-print "Built:", mtmod, "(%d bytes)" % os.path.getsize(mtmod)
+print("Built: %s (%d bytes)" % (mtmod, os.path.getsize(mtmod)))
